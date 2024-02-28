9aa0a96bd53d1ab69033f386b11d43213fb6f9ef
mikaa2001 <mikaa2001@gmail.com>
2024-02-28 09:46:17 +0500

    Output control

---
+++ OutputControl.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutputControl
+{
+	internal class OutputControl
+	{
+		static void Main(string[] args)
+		{
+			string input = "";
+			string commandExit = "exit";
+
+			while (input != commandExit)
+			{
+				Console.Write($"Введите слово или {commandExit} для завершения программы: ");
+				input = Console.ReadLine();
+				Console.WriteLine($"Вы ввели: {input}");
+			}
+
+			Console.WriteLine("Программа завершена.");
+		}
+	}
+}
